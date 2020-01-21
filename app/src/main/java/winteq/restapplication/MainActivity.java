8e114b97c8aa3cfc6b077c42b7ece82277c94511
mrzkybmsty <mohammad.bimasetya@gmail.com>
2020-01-21 13:59:09 +0700

    login

--- app/src/main/java/winteq/restapplication/MainActivity.java
+++ app/src/main/java/winteq/restapplication/MainActivity.java
@@ -1,14 +1,30 @@
 package winteq.restapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.btn_intoLogin);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
