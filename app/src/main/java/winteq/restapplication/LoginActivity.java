abc8f0ee72900459e6ecfd9bcfafbbd98f605301
mrzkybmsty <mohammad.bimasetya@gmail.com>
2020-01-21 22:13:43 +0700

    Update Home

--- app/src/main/java/winteq/restapplication/LoginActivity.java
+++ app/src/main/java/winteq/restapplication/LoginActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ImageView ivBack;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +19,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         ivBack = findViewById(R.id.ivBack);
+        btnLogin = findViewById(R.id.btnLogin);
 
         ivBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                 startActivity(intent);
                 finish();
             }
